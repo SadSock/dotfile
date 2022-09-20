5374f688fe75cc59ec29d295032574e5d099689c
blueSock <mantis-shrimp@outlook.com>
2022-09-21 00:35:43 +0800

    update

--- config.fish
+++ config.fish
@@ -8,3 +8,6 @@ alias  magit="emacsclient -t -c -e \"(progn (magit-status) (delete-other-windows
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+set --universal tide_character_icon '~>'
+# set --universal tide_character_icon '🐟'
