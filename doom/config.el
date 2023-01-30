c2bf82bc5c614d1118a70bc591a8f0c8bd8d008a
sadsock <mantis-shrimp@outlook.com>
2023-01-30 11:36:58 +0800

    update

--- doom/config.el
+++ doom/config.el
@@ -20,7 +20,7 @@
 ;;
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
-(setq doom-font (font-spec :family "monofur nerd font mono" :size 20))
+(setq doom-font (font-spec :family "monofur nerd font mono" :size 38))
 ;;
 ;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
 ;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
