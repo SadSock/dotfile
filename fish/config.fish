889907284434914741411e69423b3fb642c2a5df
sadsock <mantis-shrimp@outlook.com>
2023-09-26 16:18:46 +0800

    update

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,11 @@
 #g some more ls aliases
-alias  ls="exa"
-alias  ll="exa -lF"
-alias  la="exa -la"
+
+source (/home/xwu/.nix-profile/bin/starship init fish --print-full-init | psub)
+fzf_configure_bindings
+
+alias ll='exa -lahF --icons --color-scale --group-directories-first'
+alias la='exa -a --icons --color-scale --group-directories-first'
+alias ls='exa --icons --color-scale --group-directories-first'
 alias  e="emacsclient -nw -c"
 alias  magit="emacsclient -t -c -e \"(progn (magit-status) (delete-other-windows))\""
 
