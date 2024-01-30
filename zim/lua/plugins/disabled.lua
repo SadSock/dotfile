c2c26a5e94ff15580622a47474041f2d358d56cb
SadSock <mantis-shrimp@outlook.com>
2024-01-30 10:54:31 +0800

    update

--- zim/lua/plugins/disabled.lua
+++ zim/lua/plugins/disabled.lua
@@ -1,13 +1,11 @@
 return {
-  {"nvim-neo-tree/neo-tree.nvim", enabled = false},
-
-  {
-    "echasnovski/mini.surround", enabled = false
-  },
-
-  { "folke/noice.nvim",       enabled = false },
-  {"folke/flash.nvim", enabled = false},
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "echasnovski/mini.surround", enabled = false },
+  { "folke/noice.nvim", enabled = false },
+  { "folke/flash.nvim", enabled = false },
   --{ "stevearc/conform.nvim",  enabled = false },
   { "echasnovski/mini.pairs", enabled = false },
   { "echasnovski/mini.bufremove", enabled = false },
+  { "rafamadriz/friendly-snippets", enabled = false },
+  { "MunifTanjim/nui.nvim", enabled = false },
 }
