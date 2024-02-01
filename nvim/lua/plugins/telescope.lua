f341b41fab6783108960d0cf7fc18a78623046d2
sadsock <mantis-shrimp@outlook.com>
2024-02-02 00:42:06 +0800

    update

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,17 @@
+return {
+    {
+	"nvim-telescope/telescope.nvim",
+	lazy = true,
+	cmd = "Telescope",
+	config = require("configs.telescope"),
+	dependencies = {
+	    { "nvim-tree/nvim-web-devicons" },
+	    { "nvim-lua/plenary.nvim" },
+	    { "debugloop/telescope-undo.nvim" },
+	    { "jvgrootveld/telescope-zoxide" },
+	    { "nvim-telescope/telescope-frecency.nvim" },
+	    { "nvim-telescope/telescope-live-grep-args.nvim" },
+	    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	},
+    },
+}
