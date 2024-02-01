18877974961dd315ff22738ec2792bba57b94ccb
sadsock <mantis-shrimp@outlook.com>
2024-02-01 05:24:26 +0000

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -65,50 +65,7 @@ vim.wo.relativenumber = true
 
 vim.opt.shiftwidth = 4
 
-
-local cmp = require'cmp'
-local lspkind = require('lspkind')
-
-cmp.setup({
-    completion= {autocomplete = false},
-    snippet = {
-	expand = function(args)
-	    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-	end,
-    },
-    window = {
-	--completion = cmp.config.window.bordered(),
-	--documentation = cmp.config.window.bordered(),
-    },
-    mapping = cmp.mapping.preset.insert({
-	['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-	['<Tab>'] = function(fallback)
-        if cmp.visible() then
-          cmp.select_next_item()
-        else
-          fallback()
-        end
-      end
-    }),
-    sources = {
-	{ name = 'nvim_lsp' },
-	{ name = 'luasnip' }, -- For luasnip users.
-	{ name = 'buffer' },
-	{ name = 'path' },
-    },
-    view = {
-	entries = "custom" -- can be "custom", "wildmenu" or "native"
-    },
-    formatting = {
-	format = lspkind.cmp_format(),
-    },
-})
-
--- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-require('lspconfig').clangd.setup {
-    capabilities = capabilities
-}
-require('lspconfig').lua_ls.setup {
-    capabilities = capabilities
-}
+-- disable line wrap 
+vim.wo.wrap = false
+vim.wo.linebreak = true
+vim.wo.list = false -- extra option I set in addition to the ones in your question
