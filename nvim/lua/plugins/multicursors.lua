41d70b48fb2149e6e426219ed5b350c104023269
sadsock <mantis-shrimp@outlook.com>
2024-02-07 09:12:53 +0000

    update

---
+++ nvim/lua/plugins/multicursors.lua
@@ -0,0 +1,34 @@
+return {
+    {
+	"smoka7/multicursors.nvim",
+	event = "VeryLazy",
+	dependencies = {
+	    'smoka7/hydra.nvim',
+	},
+	opts = {
+
+	    hint_config = {
+		border = 'rounded',
+		position = 'bottom-right',
+	    },
+	    generate_hints = {
+		normal = true,
+		insert = true,
+		extend = true,
+		config = {
+		    column_count = 1,
+		},
+	    },
+
+	},
+	cmd = { 'MCstart', 'MCvisual', 'MCclear', 'MCpattern', 'MCvisualPattern', 'MCunderCursor' },
+	keys = {
+	    {
+		mode = { 'v', 'n' },
+		'<Leader>m',
+		'<cmd>MCstart<cr>',
+		desc = 'Multicursors',
+	    },
+	},
+    }
+}
