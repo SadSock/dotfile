aaa3f3764ecb16e616482fef5370472bc2d16c46
sadsock <mantis-shrimp@outlook.com>
2024-11-14 07:10:34 +0000

    update

--- init.bash
+++
@@ -1,5 +0,0 @@
-export FZF_CTRL_R_OPTS="--preview 'echo {}' --preview-window=top,wrap"
-export FZF_DEFAULT_COMMAND='fd --type f --hidden --no-ignore --exclude={.git,\.cache*,\.ccache*}'
-export STARSHIP_CONFIG="~/.config/starship.toml";
-export FZF_ALT_C_COMMAND='fd --hidden --type directory --exclude ".git"'
-export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
