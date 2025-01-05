2af13688bfc3b289e1d3426a0f2efe109465362e
sadsock <mantis-shrimp@outlook.com>
2025-01-05 11:56:42 +0000

    completion

---
+++ nvim/lua/plugins/completion.lua
@@ -0,0 +1,18 @@
+-- 代码补全
+
+return{{
+    "hrsh7th/nvim-cmp",
+    lazy = true,
+    event = "InsertEnter",
+    config = require("configs.cmp"),
+    dependencies = {
+	{
+	},
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "hrsh7th/cmp-nvim-lua" },
+	{"onsails/lspkind.nvim"},
+	{ "hrsh7th/cmp-path" },
+	{ "hrsh7th/cmp-buffer" },
+    },
+}
+}
